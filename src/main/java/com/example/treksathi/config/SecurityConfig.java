8f60d4a8b15b32c243d55e19f74942cbec78e51e
bhattaraiprati <pratikb957@gmail.com>
2026-02-11 22:44:54 +0545

    updated the configuration in render.ymal

--- src/main/java/com/example/treksathi/config/SecurityConfig.java
+++ src/main/java/com/example/treksathi/config/SecurityConfig.java
@@ -1,124 +1,124 @@
-package com.example.treksathi.config;
+    package com.example.treksathi.config;
 
-import com.example.treksathi.service.UserDetailsServiceImpl;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.util.StringUtils;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+    import com.example.treksathi.service.UserDetailsServiceImpl;
+    import jakarta.servlet.ServletException;
+    import jakarta.servlet.http.HttpServletRequest;
+    import jakarta.servlet.http.HttpServletResponse;
+    import lombok.RequiredArgsConstructor;
+    import lombok.extern.slf4j.Slf4j;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.http.HttpStatus;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.http.SessionCreationPolicy;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+    import org.springframework.util.StringUtils;
+    import org.springframework.web.client.RestTemplate;
+    import org.springframework.web.cors.CorsConfiguration;
+    import org.springframework.web.cors.CorsConfigurationSource;
+    import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+    import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.Arrays;
+    import java.io.IOException;
+    import java.lang.reflect.Array;
+    import java.util.Arrays;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-@Slf4j
-public class SecurityConfig {
+    @Configuration
+    @EnableWebSecurity
+    @RequiredArgsConstructor
+    @Slf4j
+    public class SecurityConfig {
 
-    private final JwtAuthFilter jwtAuthFilter;
+        private final JwtAuthFilter jwtAuthFilter;
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder(12);
+        }
+
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource(){
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE","PUT", "PATCH"));
+            configuration.setAllowedHeaders(Arrays.asList("*"));
+            configuration.setAllowCredentials(true);
+            configuration.setMaxAge(3600L);
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return  source;
+        }
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2SuccessHandler oAuth2SuccessHandler) throws Exception {
+
+            http.csrf(customizer -> customizer.disable())
+                    .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers( "/auth/**", "/event/registration/success", "/event/registration/failure").permitAll()
+                            .requestMatchers("/v3/api-docs/auth/**").permitAll()
+                            .requestMatchers("/auth/**",  "/public/stats").permitAll()
+                            .requestMatchers("/actuator/health", "/api/actuator/health").permitAll()
+                            .requestMatchers("/auth/**").permitAll()
+                            .requestMatchers("/oauth2/**", "/oauth2/**", "/login/oauth2/**", "/oauth2/authorization/**").permitAll()
+                            .requestMatchers(
+                                    "/v3/api-docs/**",
+                                    "/swagger-ui/**",
+                                    "/swagger-ui.html",
+                                    "/swagger-resources/**",
+                                    "/webjars/**"
+                            ).permitAll()
+                            .requestMatchers("/ws/**").permitAll()
+                            .requestMatchers("/ws/info/**").permitAll()  // Especially this one
+                            .requestMatchers("/api/ws/**").permitAll()
+                            .requestMatchers("/api/ws/info/**").permitAll()
+
+                            .requestMatchers("/admin/**").hasRole("ADMIN")
+                            .requestMatchers("/organizer/**").hasRole("ORGANIZER")
+                            .requestMatchers("/hiker/**", "/ai/**").hasRole("HIKER")
+
+                            // Multiple roles allowed
+                            .requestMatchers("/event/**", "/chat/**", "/notifications/**", "/reviews/**", "/search/**").hasAnyRole("ADMIN", "ORGANIZER", "HIKER")
+
+                            .anyRequest().authenticated())
+                    .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                    .oauth2Login(oAuth2->oAuth2.failureHandler(
+                            (request, response, exception) -> {
+                                log.error("OAuth error: {}", exception.getMessage());
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
+                            }
+                    ).successHandler(oAuth2SuccessHandler))
+                    .formLogin(form -> form.disable())
+                    .httpBasic(basic -> basic.disable())
+                    .exceptionHandling(ex -> ex
+                            .authenticationEntryPoint(
+                                    new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                            )
+                    )
+                    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+            return http.build();
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+            return config.getAuthenticationManager();
+        }
+
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
+
+
+
+
     }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource(){
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE","PUT", "PATCH"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return  source;
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, OAuth2SuccessHandler oAuth2SuccessHandler) throws Exception {
-
-        http.csrf(customizer -> customizer.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers( "/auth/**", "/event/registration/success", "/event/registration/failure").permitAll()
-                        .requestMatchers("/v3/api-docs/auth/**").permitAll()
-                        .requestMatchers("/auth/**",  "/public/stats").permitAll()
-                        .requestMatchers("/actuator/health", "/api/actuator/health").permitAll()
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/oauth2/**", "/oauth2/**", "/login/oauth2/**", "/oauth2/authorization/**").permitAll()
-                        .requestMatchers(
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger-ui.html",
-                                "/swagger-resources/**",
-                                "/webjars/**"
-                        ).permitAll()
-                        .requestMatchers("/ws/**").permitAll()
-                        .requestMatchers("/ws/info/**").permitAll()  // Especially this one
-                        .requestMatchers("/api/ws/**").permitAll()
-                        .requestMatchers("/api/ws/info/**").permitAll()
-
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/organizer/**").hasRole("ORGANIZER")
-                        .requestMatchers("/hiker/**", "/ai/**").hasRole("HIKER")
-
-                        // Multiple roles allowed
-                        .requestMatchers("/event/**", "/chat/**", "/notifications/**", "/reviews/**", "/search/**").hasAnyRole("ADMIN", "ORGANIZER", "HIKER")
-
-                        .anyRequest().authenticated())
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login(oAuth2->oAuth2.failureHandler(
-                        (request, response, exception) -> {
-                            log.error("OAuth error: {}", exception.getMessage());
-                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
-                        }
-                ).successHandler(oAuth2SuccessHandler))
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable())
-                .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(
-                                new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
-                        )
-                )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-
-
-
-}
